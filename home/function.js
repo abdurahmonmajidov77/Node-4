f1aa7681e7300b532c5422ff039ca6110cd5480b
Abdurahmon Majidov <majidovabdurahmon573@gmail.com>
2023-02-14 13:14:29 +0500

    changes json

---
+++ home/function.js
@@ -0,0 +1,3 @@
+module.exports =  function CreateList(data) {
+    return`<ul>${data.map(elem=> {`<li><h2>${elem.title}</h2><p>${elem.body}</p></li>`})}</ul>`.replaceAll(',','')
+}
---
+++ home/index.js
@@ -0,0 +1,20 @@
+const http = require('http')
+const url = require('url')
+const fs = require('fs')
+const ListElem = require('./function.js')
+
+const Home = fs.readFileSync('./home.html', 'utf-8', (err,data) => {
+    if(err) throw err
+    return data
+})
+const Data = fs.readFileSync('./back.json','utf-8', (err,data) => {
+    if(err) throw err
+    return data
+})
+const a= JSON.parse(Data)
+
+http.createServer(function(req,res){
+    res.writeHead(200, {'Content-Type':'text/html'})
+    res.write(ListElem(a))
+    res.end(Home)
+}).listen(1001)
